df144e7bf861ec2b885ade6e4fd32072c088edd2
kayleecragg <kayleeanncragg@gmail.com>
2026-06-17 14:10:47 +1000

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        L, totalNum, averageSum = 0, 0, 0
+        for R in range(len(arr)):
+            # add right pointer value to sum
+            averageSum += arr[R]
+            # if the window is size k
+            if R - L + 1 == k:
+                # and the average is greater than or equal to threshold
+                if (averageSum / k) >= threshold:
+                    totalNum += 1
+                    averageSum -= arr[L]
+                    L += 1
+                
+                # if the average is not greater than or equal, 
+                # means window has to move
+                else:
+                    averageSum -= arr[L]
+                    L += 1
+
+        return totalNum
